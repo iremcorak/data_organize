07da28db2f2603637fff8978f0f39f2ab14edfe6
iremcorak <iremcorak@gmail.com>
2024-09-03 15:58:43 +0300

    organize method created, folders scanned, xml files path found.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+import shutil
+import xml.etree.ElementTree as ET
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def organize_images_by_label(root_dir, output_dir):
+    same_data_count = 0
+    # scanning folders
+    for subdir, _, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith(".png") or file.endswith(".jpg"):
+                # found path to xml file
+                xml_file_name = os.path.splitext(file)[0] + ".xml"
+                xml_path = os.path.join(subdir, xml_file_name)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+                if not os.path.exists(xml_path):
+                    print(f"Uyarı: XML dosyası bulunamadı - {file}")
+                    continue
